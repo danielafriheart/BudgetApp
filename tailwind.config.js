154b84999b682320af82e52c77730caabfa171bf
Daniel Esuola <esuoladaniel002@gmail.com>
2022-10-17 16:30:15 +0100

    installed Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", //Configure Tailwind css
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
